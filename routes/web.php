e27f0825068afa5c7e3d765926bc46de19cc762b
Makena-Maryann <maryann.makena00@gmail.com>
2021-08-13 11:24:36 +0300

    Service Container Fundamentals

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,17 @@ Route::post('/contact', 'ContactController@store');
 
 Route::get('/payments/create', 'PaymentsController@create')->middleware('auth');
 Route::post('/payments', 'PaymentsController@store')->middleware('auth');
+
+Route::get('/', function () {
+      $container = new \App\Container();
+
+      $container->bind('example', function (){
+           return new \App\Example();
+      });
+
+      $example = $container->resolve('example');
+
+      $example->go();
+
+});
+
