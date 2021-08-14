23aea8eeae39e3b4a536d34358d1fb9833942d3f
Makena-Maryann <maryann.makena00@gmail.com>
2021-08-14 22:09:26 +0300

    Laravel Facades Demystified

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Example;
-use Illuminate\Http\Request;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Request;
 
 class PagesController extends Controller
 {
-    public function home()
+    public function home(Filesystem $file)
     {
-        ddd(resolve('App\Example'), resolve('App\Example'));
+        Cache::remember('foo', 60, fn() => 'foobar');
+
+        return Cache::get('foo');
+        // return $file->get(public_path('index.php'));
+        // return File::get(public_path('index.php'));
+        // return Request::input('name');
+        // return View::make('welcome');
     }
 }
