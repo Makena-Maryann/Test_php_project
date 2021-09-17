575c459fbdc893608ceb8a64b7ca3a7b1f448d16
Makena-Maryann <maryann.makena00@gmail.com>
2021-09-17 09:53:32 +0300

    Clean up

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -25,24 +25,3 @@
         }
     } 
 </script>
-
-@forelse ($posts as $post)
-    <h2>
-      <a href="/posts/{{ $post->id }}">
-      {{ $post->title }}
-      </a>
-    </h2>  
-    <p>
-      {{ $post->body }}
-    </p> 
-
-     <a href="/posts/{{$post->id}}/edit">Edit</a> 
-
-    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST'])!!}
-          {{Form::hidden('_method', 'DELETE')}}
-          {{Form::submit('Delete')}}
-    {!!Form::close()!!} 
-
-  @empty
-    <p>No relevant posts yet!</p>            
-  @endforelse
